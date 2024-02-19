7e8698c755f75c5a32b47573dddfb7d32559d3e3
UFifty50sh <UFifty50.github@gmail.com>
2024-02-19 21:34:57 +0000

    first

---
+++ premake5.lua
@@ -0,0 +1,190 @@
+workspace "ImTex"
+    architecture "x86_64"
+    configurations { "Debug", "Release" }
+    flags { "MultiProcessorCompile" }
+
+    filter "configurations:Debug"
+        defines { "IMTEX_DEBUG" }
+        symbols "On"
+        optimize "Off"
+
+    filter "configurations:Release"
+        defines { "IMTEX_RELEASE" }
+        symbols "Off"
+        optimize "On"
+
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+group ""
+project "MicroTeX"
+    location "MicroTeX"
+    kind "StaticLib"
+    staticruntime "off"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin/intermediate/" .. outputDir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/atom/atom_basic.cpp",
+        "%{prj.name}/src/atom/atom_char.cpp",
+        "%{prj.name}/src/atom/atom_impl.cpp",
+        "%{prj.name}/src/atom/atom_matrix.cpp",
+        "%{prj.name}/src/atom/atom_row.cpp",
+        "%{prj.name}/src/atom/atom_space.cpp",
+        "%{prj.name}/src/atom/colors_def.cpp",
+        "%{prj.name}/src/atom/unit_conversion.cpp",
+        "%{prj.name}/src/box/box.cpp",
+        "%{prj.name}/src/box/box_factory.cpp",
+        "%{prj.name}/src/box/box_group.cpp",
+        "%{prj.name}/src/box/box_single.cpp",
+        "%{prj.name}/src/core/core.cpp",
+        "%{prj.name}/src/core/formula.cpp",
+        "%{prj.name}/src/core/formula_def.cpp",
+        "%{prj.name}/src/core/glue.cpp",
+        "%{prj.name}/src/core/localized_num.cpp",
+        "%{prj.name}/src/core/macro.cpp",
+        "%{prj.name}/src/core/macro_def.cpp",
+        "%{prj.name}/src/core/macro_impl.cpp",
+        "%{prj.name}/src/core/parser.cpp",
+        "%{prj.name}/src/fonts/alphabet.cpp",
+        "%{prj.name}/src/fonts/font_basic.cpp",
+        "%{prj.name}/src/fonts/font_info.cpp",
+        "%{prj.name}/src/fonts/fonts.cpp",
+        "%{prj.name}/src/utils/string_utils.cpp",
+        "%{prj.name}/src/utils/utf.cpp",
+        "%{prj.name}/src/utils/utils.cpp",
+        "%{prj.name}/src/res/builtin/formula_mappings.res.cpp",
+        "%{prj.name}/src/res/builtin/symbol_mapping.res.cpp",
+        "%{prj.name}/src/res/builtin/tex_param.res.cpp",
+        "%{prj.name}/src/res/builtin/tex_symbols.res.cpp",
+        "%{prj.name}/src/res/font/bi10.def.cpp",
+        "%{prj.name}/src/res/font/bx10.def.cpp",
+        "%{prj.name}/src/res/font/cmbsy10.def.cpp",
+        "%{prj.name}/src/res/font/cmbx10.def.cpp",
+        "%{prj.name}/src/res/font/cmbxti10.def.cpp",
+        "%{prj.name}/src/res/font/cmex10.def.cpp",
+        "%{prj.name}/src/res/font/cmmi10.def.cpp",
+        "%{prj.name}/src/res/font/cmmi10_unchanged.def.cpp",
+        "%{prj.name}/src/res/font/cmmib10.def.cpp",
+        "%{prj.name}/src/res/font/cmmib10_unchanged.def.cpp",
+        "%{prj.name}/src/res/font/cmr10.def.cpp",
+        "%{prj.name}/src/res/font/cmss10.def.cpp",
+        "%{prj.name}/src/res/font/cmssbx10.def.cpp",
+        "%{prj.name}/src/res/font/cmssi10.def.cpp",
+        "%{prj.name}/src/res/font/cmsy10.def.cpp",
+        "%{prj.name}/src/res/font/cmti10.def.cpp",
+        "%{prj.name}/src/res/font/cmti10_unchanged.def.cpp",
+        "%{prj.name}/src/res/font/cmtt10.def.cpp",
+        "%{prj.name}/src/res/font/dsrom10.def.cpp",
+        "%{prj.name}/src/res/font/eufb10.def.cpp",
+        "%{prj.name}/src/res/font/eufm10.def.cpp",
+        "%{prj.name}/src/res/font/i10.def.cpp",
+        "%{prj.name}/src/res/font/moustache.def.cpp",
+        "%{prj.name}/src/res/font/msam10.def.cpp",
+        "%{prj.name}/src/res/font/msbm10.def.cpp",
+        "%{prj.name}/src/res/font/r10.def.cpp",
+        "%{prj.name}/src/res/font/r10_unchanged.def.cpp",
+        "%{prj.name}/src/res/font/rsfs10.def.cpp",
+        "%{prj.name}/src/res/font/sb10.def.cpp",
+        "%{prj.name}/src/res/font/sbi10.def.cpp",
+        "%{prj.name}/src/res/font/si10.def.cpp",
+        "%{prj.name}/src/res/font/special.def.cpp",
+        "%{prj.name}/src/res/font/ss10.def.cpp",
+        "%{prj.name}/src/res/font/stmary10.def.cpp",
+        "%{prj.name}/src/res/font/tt10.def.cpp",
+        "%{prj.name}/src/res/parser/font_parser.cpp",
+        "%{prj.name}/src/res/parser/formula_parser.cpp",
+        "%{prj.name}/src/res/reg/builtin_font_reg.cpp",
+        "%{prj.name}/src/res/reg/builtin_syms_reg.cpp",
+        "%{prj.name}/src/res/sym/amsfonts.def.cpp",
+        "%{prj.name}/src/res/sym/amssymb.def.cpp",
+        "%{prj.name}/src/res/sym/base.def.cpp",
+        "%{prj.name}/src/res/sym/stmaryrd.def.cpp",
+        "%{prj.name}/src/res/sym/symspecial.def.cpp",
+        "%{prj.name}/src/latex.cpp",
+        "%{prj.name}/src/render.cpp",
+    }
+
+    includedirs {
+        "%{prj.name}/src"
+    }
+
+    links {
+        "tinyxml2"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines {
+            "BUILD_WIN32",
+            "_HAS_STD_BYTE=0"
+        }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+
+        files {
+            "%{prj.name}/src/platform/cairo/graphic_cairo.cpp",
+        }
+
+        defines {
+            "BUILD_GTK"
+        }
+
+    filter "toolset:msc*"
+        buildoptions { 
+            "/utf-8",
+            "/Zc:preprocessor"
+        }
+
+
+project "ImTex"
+    location "ImTex"
+    kind "ConsoleApp"
+    staticruntime "off"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin/intermediate" .. outputDir .. "/%{prj.name}")
+
+    files {
+        "src/main.cpp"
+    }
+
+    includedirs {
+        "src"
+    }
+
+    links {
+        "MicroTeX",
+        "tinyxml2"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines {
+            "BUILD_WIN32",
+            "_HAS_STD_BYTE=0"
+        }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+
+        defines {
+            "BUILD_GTK"
+        }
+
+    filter "toolset:msc*"
+        buildoptions { 
+            "/utf-8",
+            "/Zc:preprocessor"
+        }
